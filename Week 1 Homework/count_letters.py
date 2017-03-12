4d6c4f058b402978e867f0068c30338389fce616
David Mwangi Mathenge <david.mathenge98@gmail.com>
2017-03-13 00:04:55 +0300

    The programs counts all the letters in a sentence and returns a dictonary with letters as the key and letter count as the value

---
+++ Week 1 Homework/count_letters.py
@@ -0,0 +1,34 @@
+from string import ascii_letters
+
+sentence = 'Jim quickly realized that the beautiful gowns are expensive'
+
+# Create your function here!
+
+def counter(sentence):
+
+    alphabet = ascii_letters
+    
+    count_letters = {}
+    #write your code here!
+    
+    count = 0
+    
+    for letter in sentence:
+        
+        if letter in alphabet:
+            
+            if letter in count_letters:
+                
+                count_letters[letter] += 1
+    
+            else:
+                
+                count += 1
+                count_letters[letter] = count
+                count = 0
+            
+    return count_letters
+
+
+
+print(counter(sentence))

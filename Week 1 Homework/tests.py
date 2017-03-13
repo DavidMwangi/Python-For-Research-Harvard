74b120cde1ea9173e665bbe8701ac62371a4268b
David Mwangi Mathenge <david.mathenge98@gmail.com>
2017-03-13 12:13:40 +0300

    Friendly bears greetings test

---
+++ Week 1 Homework/tests.py
@@ -0,0 +1,9 @@
+bears = {"Grizzly":"angry", "Brown":"friendly", "Polar":"friendly"}
+
+for bear in bears:
+    
+  if bears[bear] == "friendly":
+      print("Hello, "+bear+" bear!")
+  else:
+      print("odd")
+
